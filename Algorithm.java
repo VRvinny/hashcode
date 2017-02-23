e65a23435242ede990bd02b23738959255eef9e8
Gavin Shek <gavgav-s@hotmail.com>
2017-02-23 20:24:01 +0000

    Update

--- Algorithm.java
+++ Algorithm.java
@@ -6,16 +6,30 @@ public class Algorithm {
 
     public void algorithm() {
 
-        int[] video = new int[1];
+        int numberOfVideos = 1;
+        int numberOfRequests = 1;
+        int numberOfCaches = 1;
+
+        int[] video = new int[numberOfVideos];
         // Read videos
 
+        // cacheID, latency
         ArrayList<HashMap<Integer, Integer>> endpoint = new ArrayList<HashMap<Integer, Integer>>();
         // Read arrayList
         
-        String[] request = new String[1];
+        String[] request = new String[numberOfRequests];
         // Read requests
 
+        ArrayList<ArrayList<Integer>> cacheQueue = new ArrayList<ArrayList<Integer>>();
 
+        for (int i = 0; i < numberOfCaches; i++) {
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            for 
+
+
+            cacheQueue.add()
+            cacheQueue.get(i).sort();
+        }
 
     }
 
