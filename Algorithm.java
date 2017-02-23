0a2993156402b134942b968f07d0cde8b31bf9be
Gavin Shek <gavgav-s@hotmail.com>
2017-02-23 19:39:10 +0000

    Update

---
+++ Algorithm.java
@@ -0,0 +1,24 @@
+import Java.util.*;
+
+public class Algorithm {
+
+    // public  cacheQueue = new PriorityQueue();
+
+    public void algorithm() {
+
+        int[] video = new int[1];
+        // Read videos
+
+        ArrayList<HashMap<Integer, Integer>> endpoint = new ArrayList<HashMap<Integer, Integer>>();
+        // Read arrayList
+        
+        String[] request = new String[1];
+        // Read requests
+
+
+
+    }
+
+
+    
+}
