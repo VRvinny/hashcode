8c6736f03d105008348844afa63053631eaffca4
Anthony Cheng <anthonyhlcheng@users.noreply.github.com>
2017-02-23 20:14:53 +0000

    Finished Parser

--- readFile.java
+++ readFile.java
@@ -4,36 +4,60 @@
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
-public class readFile {
-    BufferedReader in = new BufferedReader(new FileReader("kittens.in"));
-
+public class readFile{
+    public static BufferedReader in;
+    public int[] firstLine;
+    public String[] requestDesc;
+    public ArrayList<HashMap<Integer, Integer>> endpoint;
+    public int[] video;
     private void input() {
         try {
-            int[] firstLine = new int[5];
+            firstLine = new int[5];
             String line = in.readLine();
             String[] strs = line.split(" ");
             for (int i = 0; i < 5; i++) {
                 firstLine[i] = Integer.parseInt(strs[i]);
             }
-            //HashMap <Integer, Integer> hm = new HashMap<Integer,Integer>();
-            int[] video = new int[firstLine[0]];
+            //
+            video = new int[firstLine[0]];
             String sline = in.readLine();
             String[] videos = sline.split(" ");
             for (int i = 0; i < firstLine[0]; i++){
                 //Get videos
                 video[i] = Integer.parseInt(videos[i]);
             }
-
+            endpoint = new ArrayList<HashMap<Integer, Integer>>();
             for(int i = 0; i < firstLine[1]; i++){
                 //Get Endpoints
                 String eline = in.readLine();
                 String[] endpointInput = eline.split(" ");
-                int[] endpointIn = new int[2];
+                int latencyLimit = Integer.parseInt(endpointInput[0]);
+                int cacheServer = Integer.parseInt(endpointInput[1]);
+                HashMap <Integer, Integer> hm = new HashMap<Integer,Integer>();
+                for(int j = 0; j < cacheServer; j++){
+                    String[] lline = in.readLine().split(" ");
+                    hm.put(Integer.parseInt(lline[0]), Integer.parseInt(lline[1]));
+                }
+                endpoint.add(hm);
+
+            }
+            requestDesc = new String[firstLine[2]];
+            for(int i = 0; i < firstLine[2]; i++){
+                String rline = in.readLine();
+                requestDesc[i] = rline;
             }
         } catch (Exception e) {
             System.out.println("File Not Found. Please Try again.");
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        in = new BufferedReader(new FileReader("C:\\Users\\Anthony\\workspace\\HashCode2017\\src\\kittens.in"));
+        readFile file = new readFile();
+        file.input();
+        System.out.println(file.requestDesc);
     }
 }
